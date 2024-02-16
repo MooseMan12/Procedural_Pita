22b1552e250214625a39f37e906f66de81eccae6
MooseMan12 <bairybarsinai@gmail.com>
2024-02-16 14:05:18 +0200

    added sample operators

---
+++ CursorArray.py
@@ -0,0 +1,73 @@
+bl_info = {
+    "name": "cursor array",
+    "Blender": (3,6,0),
+    "category": "Object"
+}
+
+
+import bpy
+from bpy import context
+
+class ObjectCursorArray(bpy.types.Operator):
+    """Object Cursor Array"""
+    bl_idname = "object.cursor_array"
+    bl_label = "cursor array"
+    bl_options = {"REGISTER", "UNDO"}
+    total: bpy.props.IntProperty(name = "steps", default = 2, min = 1, max = 100)
+    
+    def execute(self, context):
+        scene = context.scene
+        cursor = scene.cursor.location
+        obj = context.active_object
+
+        # Add 'total' objects into the scene
+        for i in range(self.total):
+            obj_new = obj.copy()
+            scene.collection.objects.link(obj_new)
+
+            # Now place the object in between the cursor
+            # and the active object based on 'i'
+            factor = i / self.total
+            obj_new.location = (obj.location * factor) + (cursor * (1.0 - factor))
+            obj_new.rotation_euler = (0, 0, 360*factor*2)
+        
+        return {"FINISHED"}
+    
+    
+def menu_func(self, context):
+    self.layout.operator(ObjectCursorArray.bl_idname)
+
+addon_keymaps = []
+
+def register():
+    bpy.utils.register_class(ObjectCursorArray)
+    bpy.types.VIEW3D_MT_object.append(menu_func)
+    
+    # handle the keymap
+    wm = bpy.context.window_manager
+    # Note that in background mode (no GUI available), keyconfigs are not available either,
+    # so we have to check this to avoid nasty errors in background case.
+    kc = wm.keyconfigs.addon
+    if kc:
+        km = wm.keyconfigs.addon.keymaps.new(name='Object Mode', space_type='EMPTY')
+        kmi = km.keymap_items.new(ObjectCursorArray.bl_idname, 'T', 'PRESS', ctrl=True, shift=True)
+        kmi.properties.total = 4
+        addon_keymaps.append((km, kmi))
+        kmi = km.keymap_items.new(ObjectCursorArray.bl_idname, 'T', 'PRESS', ctrl=True, shift=True, alt=True)
+        kmi.properties.total = 50
+        addon_keymaps.append((km, kmi))
+
+def unregister():
+        # Note: when unregistering, it's usually good practice to do it in reverse order you registered.
+    # Can avoid strange issues like keymap still referring to operators already unregistered...
+    # handle the keymap
+    for km, kmi in addon_keymaps:
+        km.keymap_items.remove(kmi)
+    addon_keymaps.clear()
+
+    bpy.utils.unregister_class(ObjectCursorArray)
+    bpy.types.VIEW3D_MT_object.remove(menu_func)
+    
+if __name__ == "__main__":
+    register()
+    
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Procedural_Pita
-An addon for the free open source 3d program Blender. This addon generates a pita and lets the user customize the pita.
+An addon for the free open source 3d program Blender. This addon generates a pita with toppings and lets the user customize the pita and the toppings.
+
+## links:
+
+* https://docs.blender.org/manual/en/latest/advanced/scripting/addon_tutorial.html
+* https://docs.blender.org/api/current/info_api_reference.html
+* https://docs.blender.org/api/current/index.html
+* https://docs.blender.org/api/current/info_best_practice.html
+* https://docs.blender.org/api/current/info_tips_and_tricks.html
+* https://docs.blender.org/manual/en/latest/index.html
+* https://docs.blender.org/manual/en/latest/editors/preferences/addons.html
+* https://docs.blender.org/manual/en/dev/advanced/command_line/launch/macos.html
+
+
+## Open Blender From the Terminal
+
+### Linux
+
+```bash
+$ blender
+
+# -- or --
+
+$ cd <blender installation directory>
+$ ./blender
+```
+
+### MacOS
+
+```bash
+$ blender
+```
