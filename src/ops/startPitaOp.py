89004a2d1952a6e1b9e2a1ea8be5c7c6eebcef3c
MooseMan12 <bairybarsinai@gmail.com>
2024-04-05 14:58:33 +0300

    some changes

--- src/ops/startPitaOp.py
+++ src/ops/startPitaOp.py
@@ -9,17 +9,10 @@ class PR_OT_startPita(bpy.types.Operator):
 
     def execute(self, context):
         print("hello 3d world! I am a pita and I am procedural")
-        falafelPath = Path( os.path.join(__file__, "..", "..", "data", "procedural_pita-falafel_model.blend") ).resolve()
-        print(falafelPath)
+        modelspath = Path( os.path.join(__file__, "..", "..", "data", "models.blend") ).resolve()
+        print(modelspath)
 
-#        with bpy.data.libraries.load(falafelPath.absolute()) as (data_from, data_to):
-#            data_to.objects = data_from.objects
-#        
-#        for obj in data_to.objects:
-#            bpy.context.scene.collection.objects.link(obj)
-
-        with bpy.data.libraries.load(str(falafelPath)) as (data_from, data_to):
-            for attr in dir(data_to):
-                setattr(data_to, attr, getattr(data_from, attr))
+        with bpy.data.libraries.load(str(modelspath)) as (data_from, data_to):
+            data_to.meshes = data_from.meshes
 
         return {'FINISHED'}
