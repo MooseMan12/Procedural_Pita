8b32063cfe16ea5280d8cfd63769cff9d285ad64
MooseMan12 <bairybarsinai@gmail.com>
2024-04-10 09:47:00 +0300

    there is now a toggle for the amount of falafels.it does not work though.

--- src/ops/startPitaOp.py
+++ src/ops/startPitaOp.py
@@ -12,9 +12,10 @@ class PR_OT_startPita(bpy.types.Operator):
         ('raw_falafel', 'Raw Falafel', 'a more raw, lighter, falafel')
     ),
     name = "Falafel Type", description = 'here you can choose what kind of falafel you want: burnt, raw, or regular', default = 'falafel', options={'ANIMATABLE'}, override=set(), tags=set(), update=None, get=None, set=None)
+    falafel_count: bpy.props.IntProperty(name = "falafel amount", default = 1, min = 0, max = 4)
 
     def execute(self, context):
-        print("hello 3d world! I am a pita and I am procedural")
+#        print("hello 3d world! I am a pita and I am procedural")
         modelspath = Path( os.path.join(__file__, "..", "..", "data", "models.blend") ).resolve()
         print(modelspath)
 
@@ -26,4 +27,9 @@ class PR_OT_startPita(bpy.types.Operator):
         falafelObj = data_to.objects[0]
         falafelObj.location = context.scene.cursor.location
 
+        for i in range(self.falafel_count):
+            falafelObj_new = falafelObj.copy()
+            falafelObj_new.location = falafelObj.location
+        
         return {'FINISHED'}
+ 
